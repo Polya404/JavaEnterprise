302ccf0e0881a5bf600a565b996587719e52e03e
Polina <polyapolya404@gmail.com>
2024-03-09 12:37:50 +0200

    added branches

--- homework_multithreading_1/src/main/java/org/application/App.java
+++ homework_multithreading_1/src/main/java/org/application/App.java
@@ -3,13 +3,13 @@ public class App
 {
     public static void main( String[] args ) {
         ValueCalculator calculator = new ValueCalculator();
-        calculator.operationsWithMultipleThreads();
+        calculator.operationsWithMultiplyThreads();
         calculator.setNumOfThreads(2);
-        calculator.operationsWithMultipleThreads();
+        calculator.operationsWithMultiplyThreads();
         calculator.setNumOfThreads(5);
-        calculator.operationsWithMultipleThreads();
+        calculator.operationsWithMultiplyThreads();
         calculator.setNumOfThreads(10);
-        calculator.operationsWithMultipleThreads();
+        calculator.operationsWithMultiplyThreads();
 
     }
 }
