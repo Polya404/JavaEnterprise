b1807f7d74107306e57804cbe21cb30fda0c9d9a
Polina <polyapolya404@gmail.com>
2024-03-22 19:03:08 +0200

    added homework

---
+++ homework_spring-mvc/EndServer/src/main/java/org/application/end/EndServerApplication.java
@@ -0,0 +1,13 @@
+package org.application.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndServerApplication.class, args);
+    }
+
+}
